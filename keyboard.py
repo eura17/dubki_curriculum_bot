20f6cda5a88a9c4ee501883a22f38bd7a0f1a072
eura17 <55353204+eura17@users.noreply.github.com>
2020-09-01 22:01:50 +0300

    added slavyanskiy bulvar

--- keyboard.py
+++ keyboard.py
@@ -15,7 +15,7 @@ class KeyboardMenu:
         self.keyboard.add(KeyboardButton('Автобусы'))
         self.keyboard.add(KeyboardButton('Славянки'))
         self.keyboard.add(KeyboardButton('Электрички'))
-        self.keyboard.add(KeyboardButton('Файл'))
+        self.keyboard.add(KeyboardButton('Файл с расписанием'))
         return self.keyboard
 
     def buses_message_menu(self):
@@ -25,9 +25,12 @@ class KeyboardMenu:
         self.keyboard.add(KeyboardButton('Главное меню'))
         return self.keyboard
 
-    def pre_suburbans_after_buses_message_menu(self):
+    def pre_suburbans_after_buses_message_menu(self, buses_direction: str):
         self.reset_keyboard()
-        self.keyboard.add(KeyboardButton('Посмотреть пересадки на электрички'))
+        if buses_direction == 'Дубки-Одинцово':
+            self.keyboard.add(KeyboardButton('Посмотреть пересадки на электрички'))
+        elif buses_direction == 'Одинцово-Дубки':
+            self.keyboard.add(KeyboardButton('Посмотреть пересадки с электричек'))
         self.keyboard.add(KeyboardButton('Главное меню'))
         return self.keyboard
 
@@ -35,6 +38,7 @@ class KeyboardMenu:
         self.reset_keyboard()
         if buses_direction == 'Дубки-Одинцово':
             self.keyboard.add(KeyboardButton('Одинцово ---> Кунцево'),
+                              KeyboardButton('Одинцово ---> Славянский бульвар'),
                               KeyboardButton('Одинцово ---> Фили'))
             self.keyboard.add(KeyboardButton('Одинцово ---> Беговая'),
                               KeyboardButton(
@@ -44,6 +48,7 @@ class KeyboardMenu:
                 'Белорусский вокзал ---> Одинцово'),
                               KeyboardButton('Беговая ---> Одинцово'))
             self.keyboard.add(KeyboardButton('Фили ---> Одинцово'),
+                              KeyboardButton('Славянский бульвар ---> Одинцово'),
                               KeyboardButton('Кунцево ---> Одинцово'))
         self.keyboard.add(KeyboardButton('Главное меню'))
         return self.keyboard
@@ -64,19 +69,25 @@ class KeyboardMenu:
     def suburbans_message_menu(self):
         self.reset_keyboard()
         self.keyboard.add(KeyboardButton('Одинцово ---> Кунцево'),
-                          KeyboardButton('Одинцово ---> Фили'))
-        self.keyboard.add(KeyboardButton('Одинцово ---> Беговая'),
-                          KeyboardButton('Одинцово ---> Белорусский вокзал'))
-        self.keyboard.add(KeyboardButton('Белорусский вокзал ---> Одинцово'),
-                          KeyboardButton('Беговая ---> Одинцово'))
-        self.keyboard.add(KeyboardButton('Фили ---> Одинцово'),
                           KeyboardButton('Кунцево ---> Одинцово'))
+        self.keyboard.add(KeyboardButton('Одинцово ---> Славянский бульвар'),
+                          KeyboardButton('Славянский бульвар ---> Одинцово'))
+        self.keyboard.add(KeyboardButton('Одинцово ---> Фили'),
+                          KeyboardButton('Фили ---> Одинцово'))
+        self.keyboard.add(KeyboardButton('Одинцово ---> Беговая'),
+                          KeyboardButton('Беговая ---> Одинцово'))
+        self.keyboard.add(KeyboardButton('Одинцово ---> Белорусский вокзал'),
+                          KeyboardButton('Белорусский вокзал ---> Одинцово'))
         self.keyboard.add(KeyboardButton('Главное меню'))
         return self.keyboard
 
-    def pre_buses_after_suburbans_message_menu(self):
+    def pre_buses_after_suburbans_message_menu(self, suburbans_direction: str):
         self.reset_keyboard()
-        self.keyboard.add(KeyboardButton('Посмотреть пересадки на автобусы'))
+        suburbans_direction_from = suburbans_direction.split('-')[0]
+        if suburbans_direction_from == 'Одинцово':
+            self.keyboard.add(KeyboardButton('Посмотреть пересадки с автобусов'))
+        else:
+            self.keyboard.add(KeyboardButton('Посмотреть пересадки на автобусы'))
         self.keyboard.add(KeyboardButton('Главное меню'))
         return self.keyboard
 
