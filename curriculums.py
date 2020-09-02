35334d05dedcf90e21a1f40b25671012fb858ebf
eura17 <55353204+eura17@users.noreply.github.com>
2020-09-02 22:06:12 +0300

    fix

--- curriculums.py
+++ curriculums.py
@@ -27,7 +27,7 @@ class BusesCurriculum(Curriculum):
         """
         self.get_curriculum_pdf()
         curriculum_pdf = open_pdf(self.filename)
-        weekdays = iter(('saturday', 'sunday', 'weekday'))
+        weekdays = iter(('weekday','saturday', 'sunday'))
         pages = []
         is_first_page = True
         for page_pdf in curriculum_pdf:
